b104656e704eb6cadfb0762e151ed3734a9b4185
Emre Yüce <emre.yuce@simetri8.com>
2025-01-08 22:36:09 +0300

    *DeleteProduct endpoint implemented

---
+++ src/Services/Catalog/eShop-microservices.Catalog.API/Products/DeleteProduct/DeleteProductCommandHandler.cs
@@ -0,0 +1,23 @@
+﻿using BuildingBlocks.CQRS;
+using eShop_microservices.Catalog.API.Exceptions;
+using eShop_microservices.Catalog.API.Models;
+
+namespace eShop_microservices.Catalog.API.Products.DeleteProduct {
+    
+    public sealed record DeleteProductCommandRequest(Guid Id):ICommand<DeleteProductCommandResponse>;
+    public sealed record DeleteProductCommandResponse(Guid Id);
+
+    internal sealed class DeleteProductCommandHandler(IDocumentSession session) : ICommandHandler<DeleteProductCommandRequest, DeleteProductCommandResponse> {
+        public async Task<DeleteProductCommandResponse> Handle(DeleteProductCommandRequest request, CancellationToken cancellationToken) {
+            //find record from db
+            var product = await session.LoadAsync<Product>(request.Id);
+            //if not exist throw exception
+            if (product is null) throw new ProductNotFoundException(); 
+            //delete record
+            session.Delete(product);
+            await session.SaveChangesAsync();
+            //return client records id
+            return new DeleteProductCommandResponse(request.Id);
+        }
+    }
+}
---
+++ src/Services/Catalog/eShop-microservices.Catalog.API/Products/DeleteProduct/DeleteProductEndpoint.cs
@@ -0,0 +1,12 @@
+﻿
+namespace eShop_microservices.Catalog.API.Products.DeleteProduct {
+    public class DeleteProductEndpoint : ICarterModule {
+        public void AddRoutes(IEndpointRouteBuilder app) {
+            app.MapDelete("/products/{id}", async (Guid Id, ISender sender) => {
+                var response = await sender.Send(new DeleteProductCommandRequest(Id));
+                return Results.Ok(response);
+
+            });
+        }
+    }
+}
