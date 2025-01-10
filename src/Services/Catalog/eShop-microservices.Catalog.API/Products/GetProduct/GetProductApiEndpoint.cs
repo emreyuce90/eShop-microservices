dbdf9f205d338e5afc543a98f64436cdc4677bad
Emre Yüce <emre.yuce@simetri8.com>
2025-01-10 14:31:17 +0300

    *Marten pagination applied

--- src/Services/Catalog/eShop-microservices.Catalog.API/Products/GetProduct/GetProductApiEndpoint.cs
+++ src/Services/Catalog/eShop-microservices.Catalog.API/Products/GetProduct/GetProductApiEndpoint.cs
@@ -5,8 +5,8 @@ using eShop_microservices.Catalog.API.Products.Dtos;
 namespace eShop_microservices.Catalog.API.Products.GetProduct {
     public class GetProductApiEndpoint : ICarterModule {
         public void AddRoutes(IEndpointRouteBuilder app) {
-            app.MapGet("/products", async (ISender sender) => {
-                var response = await sender.Send(new GetProductsQuery());
+            app.MapGet("/products", async (ISender sender,[AsParameters] GetProductsQuery query) => {
+                var response = await sender.Send(query);
                 return Results.Ok(response);
             }).WithName("GetProduct")
             .Produces<List<ProductDto>>(StatusCodes.Status201Created)
--- src/Services/Catalog/eShop-microservices.Catalog.API/Products/GetProduct/GetProductQueryHandler.cs
+++ src/Services/Catalog/eShop-microservices.Catalog.API/Products/GetProduct/GetProductQueryHandler.cs
@@ -1,14 +1,16 @@
 ﻿using BuildingBlocks.CQRS;
 using eShop_microservices.Catalog.API.Models;
 using eShop_microservices.Catalog.API.Products.Dtos;
+using Marten.Pagination;
 
 namespace eShop_microservices.Catalog.API.Products.GetProduct {
-    public sealed record GetProductsQuery():IQuery<GetProductResponse>;
+    public sealed record GetProductsQuery(int pageSize=10,int pageNumber=1):IQuery<GetProductResponse>;
     public sealed record GetProductResponse(List<ProductDto> products);
 
     internal sealed class GetProductQueryHandler(IDocumentSession session) : IQueryHandler<GetProductsQuery, GetProductResponse> {
-        public async Task<GetProductResponse> Handle(GetProductsQuery request, CancellationToken cancellationToken) {
-          var products =  await session.Query<Product>().ToListAsync(cancellationToken);
+       
+        public async Task<GetProductResponse> Handle(GetProductsQuery query, CancellationToken cancellationToken) {
+          var products =  await session.Query<Product>().ToPagedListAsync(query.pageNumber, query.pageSize, cancellationToken);
             var dto = products.Select(p => new ProductDto() { Category = p.Category, Description = p.Description, Id = p.Id, ImageUrl = p.ImageUrl, Name = p.Name, Price = p.Price, Stock = p.Stock });
             return new GetProductResponse(dto.ToList());
         }
